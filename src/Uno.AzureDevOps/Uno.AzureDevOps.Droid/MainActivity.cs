67bb3c94b5e73a83d03fec9f861e2d19ffbec8ba
Vincent Castagna <vincent.castagna@nventive.com>
2019-06-14 14:08:21 -0400

    Added Xamarin Essentials package, updated UNO & added necessarypermission for android

--- src/Uno.AzureDevOps/Uno.AzureDevOps.Droid/MainActivity.cs
+++ src/Uno.AzureDevOps/Uno.AzureDevOps.Droid/MainActivity.cs
@@ -1,16 +1,23 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Android.Views;
 
 namespace Uno.AzureDevOps.Droid
 {
 	[Activity(
-			MainLauncher = true,
-			ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize,
-			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
-		)]
+		MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize,
+		WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+	)]
 	public class MainActivity : Windows.UI.Xaml.ApplicationActivity
 	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			//Init Xamarin essentials is needed for Android only
+			Xamarin.Essentials.Platform.Init(this, bundle);
+		}
 	}
 }
 
