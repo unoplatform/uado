4f1d1e961908d100ce5a209721941773c0035229
Vincent Castagna <vincent.castagna@nventive.com>
2019-06-17 15:53:38 -0400

    Fixed bug crash and error message when selecting pbi w/o connection

--- src/Uno.AzureDevOps/Uno.AzureDevOps.Views/Content/ProjectItemDetails.xaml.cs
+++ src/Uno.AzureDevOps/Uno.AzureDevOps.Views/Content/ProjectItemDetails.xaml.cs
@@ -20,7 +20,9 @@ namespace Uno.AzureDevOps.Views.Content
 
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 			(DataContext as ProjectItemDetailsPageViewModel).OnNavigatedTo(e.Parameter as RichWorkItem);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 			base.OnNavigatedTo(e);
 		}
 
