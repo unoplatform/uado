9e9b9ca808f28a2454b7e0450249bc7cbb688867
Umar3x <castagna.vin@gmail.com>
2019-09-12 21:45:51 -0400

    Added ios and android redirect to stores for wasm

---
+++ src/Uno.AzureDevOps/Uno.AzureDevOps.Wasm/WasmScripts/redirect.js
@@ -0,0 +1,40 @@
+﻿function getMobileOperatingSystem() {
+
+    var userAgent = navigator.userAgent;
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
+
+
+function RedirectToStores() {
+
+    let os = getMobileOperatingSystem();
+    let unoPlatform = "https://platform.uno/code-samples/";
+
+    if (os == "Android") {
+        if (confirm("Download the application for a better experience.")) {
+            window.location.href = "https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=uno.platform.azuredevops&ddl=1&pcampaignid=web_ddl_1";
+        } else {
+            window.location.href = unoPlatform;
+        }
+    } else if (os == "iOS") {
+        if (confirm("Download the application for a better experience.")) {
+            window.location.href = "https://apps.apple.com/fr/app/uado/id1472198775";
+        } else {
+            window.location.href = unoPlatform;
+        }
+    } else {
+        window.location.href = unoPlatform;
+    }
+}
+
+RedirectToStores();
